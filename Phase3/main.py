200ec789067a0ef6c6125e5fddab4be4b40e7559
mahdi <realmahdighazavi@gmail.com>
2023-05-05 21:26:34 +0430

    [fix]: fixing a small bug in the fa initialization

--- Phase3/main.py
+++ Phase3/main.py
@@ -12,13 +12,16 @@ def set_of_reachables(transitions: List[Tuple[str, str, str]], state: str, alpha
             next_states.add(transition[2])
     return next_states
 
+
 def find_states(current: Set[str], transitions: List[Tuple[str, str, str]], alphabet: str) -> Set[str]:
     next_states = set()
     for state in current:
         next_states.update(set_of_reachables(transitions, state, alphabet))
     return next_states
 
-def final_check(in_str: str, alphabets: List[str], final_states: List[str], initial_state: str, transitions: List[Tuple[str, str, str]]) -> bool:
+
+def final_check(in_str: str, alphabets: List[str], final_states: List[str], initial_state: str,
+                transitions: List[Tuple[str, str, str]]) -> bool:
     current = set()
     current.add(initial_state)
     current.update(find_states(current, transitions, '$'))
@@ -33,7 +36,7 @@ def final_check(in_str: str, alphabets: List[str], final_states: List[str], init
     else:
         return True
 
-    
+
 def Finite_automata(json_path):
     Fa = json.load(open(json_path))
 
@@ -59,21 +62,16 @@ def Finite_automata(json_path):
         if not (i + 1) == len(Fa['states'][1: -1].split(',')):
             Fa_states = Fa_states + ','
 
-    
     Fa_alphabets = ''
     for i in range(len(Fa['input_symbols'][1: -1].split(','))):
         Fa_alphabets = Fa_alphabets + Fa['input_symbols'][1: -1].split(',')[i][1: -1]
         if not (i + 1) == len(Fa['input_symbols'][1: -1].split(',')):
             Fa_alphabets = Fa_alphabets + ','
 
-    
     Fa_transitions = []
     for state, transitions in Fa['transitions'].items():
         for symbol in transitions.keys():
             Fa_transitions.append(f'{state},{symbol},{transitions[symbol]}')
-
-
-
 
     Fa_initial_state = Fa['initial_state']
     Fa_final_states = ''
@@ -84,22 +82,21 @@ def Finite_automata(json_path):
 
     return Fa_states, Fa_alphabets, Fa_transitions, Fa_initial_state, Fa_final_states
 
+
 if __name__ == '__main__':
 
     args = sys.argv[1:]
     json_path = args[0]
-    
-    Fa_states, Fa_alphabets, Fa_transitions, Fa_initial_state, Fa_final_states = Finite_automata()
-    
-    input_states=Fa_states
-    input_alphabet=Fa_alphabets
-    input_final_states=Fa_final_states
-    input_transitions_no=len(Fa_transitions)
 
+    Fa_states, Fa_alphabets, Fa_transitions, Fa_initial_state, Fa_final_states = Finite_automata(json_path)
 
+    input_states = Fa_states
+    input_alphabet = Fa_alphabets
+    input_final_states = Fa_final_states
+    input_transitions_no = len(Fa_transitions)
 
-    input_transitions=Fa_transitions
-    
+    input_transitions = Fa_transitions
+
     target = input().strip('{} \t\n\r')
 
     start_state = input_states.split(',')[0]
