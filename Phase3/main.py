44e5cdfe3c9011bfadde6dbac6b3ef4f4f128e72
Aref alijannezhad <74285590+Arefalj@users.noreply.github.com>
2023-05-05 20:56:12 +0330

    [algoritm]: coding alghoritm phase3

--- Phase3/main.py
+++ Phase3/main.py
@@ -1,55 +1,36 @@
-import json
-import sys
-from typing import List, Set
+from typing import List, Tuple, Set
 
+def set_of_reachables(transitions: List[Tuple[str, str, str]], state: str, alphabet: str) -> Set[str]:
+    next_states = set()
+    for transition in transitions:
+        if transition[0] != state:
+            continue
+        elif transition[1] == alphabet:
+            next_states.add(transition[2])
+    return next_states
 
-class NfaSimulator:
+def find_states(current: Set[str], transitions: List[Tuple[str, str, str]], alphabet: str) -> Set[str]:
+    next_states = set()
+    for state in current:
+        next_states.update(set_of_reachables(transitions, state, alphabet))
+    return next_states
 
-    def __init__(self, alphabets: Set[str], finalStates: Set[str], initialState: str, transitions: Set[List[str]]):
-        self._alphabets = alphabets
-        self._finalStates = finalStates
-        self._initialState = initialState
-        self._transitions = transitions
-
-    def set_of_reachables(self, state: str, alphabet: str) -> Set[str]:
-        next_states = set()
-        for transition in self._transitions:
-            if transition[0] != state:
-                continue
-            elif transition[1] == alphabet:
-                next_states.add(transition[2])
-            else:
-                continue
-        return next_states
-
-    def find_states(self, current: Set[str], alphabet: str) -> Set[str]:
-        next_states = set()
-        for state in current:
-            tmp = next_states.union(self.set_of_reachables(state, alphabet))
-            next_states = set(tmp)
-        return next_states
-
-    def final_check(self, input_str: str) -> bool:
-        current = set()
-        current.add(self._initialState)
-        tmp = current.union(self.find_states(current, "$"))
-        current = set(tmp)
-        for ch in input_str:
-            if ch not in self._alphabets:
-                return False
-            else:
-                current = self.find_states(current, ch)
-                tmp = current.union(self.find_states(current, "$"))
-                current = set(tmp)
-
-        result = current.intersection(self._finalStates)
-        if len(result) == 0:
+def final_check(in_str: str, alphabets: List[str], final_states: List[str], initial_state: str, transitions: List[Tuple[str, str, str]]) -> bool:
+    current = set()
+    current.add(initial_state)
+    current.update(find_states(current, transitions, '$'))
+    for ch in in_str:
+        if ch not in alphabets:
             return False
-        else:
-            return True
+        current = find_states(current, transitions, ch)
+        current.update(find_states(current, transitions, '$'))
+    result = current.intersection(final_states)
+    if len(result) == 0:
+        return False
+    else:
+        return True
 
-
-
+    
 def Finite_automata():
     Fa = {
         "states": "{'A','B'}",
@@ -67,55 +48,61 @@ def Finite_automata():
         "initial_state": "A",
         "final_states": "{'B'}"
     }
-    Fa_states = [state[1: -1] for state in Fa['states'][1: -1].split(',')]
-    Fa_alphabets = [alphabet[1: -1] for alphabet in Fa['input_symbols'][1: -1].split(',')]
+    Fa_states = ''
+    for i in range(len(Fa['states'][1: -1].split(','))):
+        Fa_states = Fa_states + Fa['states'][1: -1].split(',')[i][1: -1]
+        if not (i + 1) == len(Fa['states'][1: -1].split(',')):
+            Fa_states = Fa_states + ','
 
-    Fa_transitions = {}
-    for key in Fa['transitions']:
-        for k in Fa['transitions'][key].keys():
-            result_states = [s[1: -1] for s in Fa['transitions'][key][k][1: -1].split(',')]
-            for result_state in result_states:
-                if (key, k) not in Fa_transitions:
-                    Fa_transitions[(key, k)] = [result_state]
-                else:
-                    Fa_transitions[(key, k)].append(result_state)
+    
+    Fa_alphabets = ''
+    for i in range(len(Fa['input_symbols'][1: -1].split(','))):
+        Fa_alphabets = Fa_alphabets + Fa['input_symbols'][1: -1].split(',')[i][1: -1]
+        if not (i + 1) == len(Fa['input_symbols'][1: -1].split(',')):
+            Fa_alphabets = Fa_alphabets + ','
+
+    
+    Fa_transitions = []
+    for state, transitions in Fa['transitions'].items():
+        for symbol in transitions.keys():
+            Fa_transitions.append(f'{state},{symbol},{transitions[symbol]}')
+
+
+
 
     Fa_initial_state = Fa['initial_state']
-    Fa_final_states = [state[1: -1] for state in Fa['final_states'][1: -1].split(',')]
+    Fa_final_states = ''
+    for i in range(len(Fa['final_states'][1: -1].split(','))):
+        Fa_final_states = Fa_final_states + Fa['final_states'][1: -1].split(',')[i][1: -1]
+        if not (i + 1) == len(Fa['final_states'][1: -1].split(',')):
+            Fa_final_states = Fa_final_states + ','
+
     return Fa_states, Fa_alphabets, Fa_transitions, Fa_initial_state, Fa_final_states
 
-
-if __name__ == "__main__":
-    # inputStates = input().strip("{} \t\n\r")
-    # inputAlphabet = input().strip("{} \t\n\r")
-    # inputFinalStates = input().strip("{} \t\n\r")
-    # inputTransitionsNo = int(input().strip("{} \t\n\r"))
+if __name__ == '__main__':
     Fa_states, Fa_alphabets, Fa_transitions, Fa_initial_state, Fa_final_states = Finite_automata()
+    
+    input_states=Fa_states
+    input_alphabet=Fa_alphabets
+    input_final_states=Fa_final_states
+    input_transitions_no=len(Fa_transitions)
 
-    inputTransitions = []
-    # for i in range(inputTransitionsNo):
-    #     inputTransitions.append(input().strip())
-    for key in Fa_transitions.keys():
-        inputTransitions.append(key)
 
-    target = input().strip("{} \t\n\r")
 
-    start_state = Fa_states[0]
-    final_states = set(Fa_final_states)
-    alphabets = set(Fa_alphabets)
+    input_transitions=Fa_transitions
+    
+    target = input().strip('{} \t\n\r')
 
-    transitions = set()
-    # for i in range(inputTransitionsNo):
-    #     temp = inputTransitions[i].split(",")
-    #     transitions.add([temp[0], temp[1], temp[2]])
-    i=0
-    for key in Fa_transitions.keys():
-        temp=inputTransitions[i]
-        transitions.add([temp[0], temp[1], temp[2]])
-        i+=1
+    start_state = input_states.split(',')[0]
+    final_states = set(input_final_states.split(','))
+    alphabets = set(input_alphabet.split(','))
 
-    simulator = NfaSimulator(alphabets, final_states, start_state, transitions)
-    if simulator.final_check(target):
+    transitions = []
+    for i in range(input_transitions_no):
+        temp = input_transitions[i].split(',')
+        transitions.append((temp[0], temp[1], temp[2]))
+
+    if final_check(target, alphabets, final_states, start_state, transitions):
         print("Accepted")
     else:
         print("Rejected")
